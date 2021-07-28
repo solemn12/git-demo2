a8f33a6e65e74ec4308ac96fe7ccbd78c671ddc3
solemn12 <chen13572806871@gmail.com>
2021-07-28 16:33:52 +0800

    first commit idea

---
+++ src/main/java/com/atguigu/GitDemo.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+    }
+}
